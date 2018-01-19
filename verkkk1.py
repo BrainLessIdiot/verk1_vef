1c1ab523e8dc0d182552583605f0d7740ce00f71
BrainLessIdiot <beggi1090@gmail.com>
2018-01-19 14:50:34 +0000

    Update verkkk1.py

--- verkkk1.py
+++ verkkk1.py
@@ -1,5 +1,5 @@
 from bottle import *
-
+import os
 @route('/')
 def index():
     return "<a href='/gaman?tala=55'>Hlekkur 0</a><a href='/sida/1'>Hlekkur 1</a><a href='/sida/2'>Hlekkur 2</a>"
@@ -11,4 +11,5 @@ def index():
 @error(404)
 def villa(error):
    return "<h2>Þessi síða er ekki til, reyndu aftur!</h2>"
-run(host="localhost", port=8080)
+run(host='0.0.0.0',port=os.environ.get('PORT'))
+#run(host="localhost", port=8080)
